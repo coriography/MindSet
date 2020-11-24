8e285f1217b7ab074b4b13d0703b158f9c0ea428
Cori Lint <corilint@gmail.com>
2020-11-24 13:42:23 -0600

    Updated category buttons to control animation and display affirmation text on click; updated HTML to correct semantic layout and delete toggle button; updated CSS heading font sizes

--- main.js
+++ main.js
@@ -8,15 +8,6 @@
 
 const body = document.getElementById("body");
 
-//Creates a global variable currentCat for "state", i.e. which category has been selected
-let currentCat = "blank";
-
-//Updates global variable based on button click
-function updateCurrentCat(data) {
-	currentCat = data;
-	console.log(currentCat);
-}
-
 //Stores all affirmations in an array of objects organized by category
 var affirmations=[
 	{
@@ -75,57 +66,9 @@ var affirmations=[
 	},
 ];
 
-
-// * CATEGORIES (MAIN) SCREEN //
-
-//Creates a button element for each item in the affirmations object where "strings" is not empty
-var catButtons = `
-	${affirmations
-		.filter(item => item.strings.length > 0)
-		.map(item => 
-			`<button type="button" id="${item.category}" class="cat-button" style="background-color: ${item.color}" onclick="updateCurrentCat('${item.category}')">
-				<h2 class="cat-text">${item.category}</h2>
-			</button>
-			`)
-		.join('')}
-`;
-
-//Displays the button elements
-let catBtnContainer = document.querySelector('#catButtons');
-catBtnContainer.innerHTML = catButtons;
-
-
-// * AFFIRMATION SCREEN //
-
-// ? Displays the current category
-// TODO: I think I need to add some kind of action... event listener? 
-// ? global var seems to update within updateCurrentCat()
-// ? but when I reference it elsewhere it's blank
-let affHeading = `Positive affirmations for ${currentCat}`;
-let affCat = document.querySelector('#affCat');
-affCat.innerHTML = affHeading;
-
-
-// ? filter for current category and assign to new var
-// ? map over obj and format to just be array of strings?
-// ? then can use that index thingy
-
-// TODO: change below from "time" to currentCat
-var currentList = affirmations.filter(item => item.category == "time")[0];
-
-
-function generateNew() {
-    //Generates a random index to retrieve a random affirmation from the list
-    let index = Math.floor(Math.random()*currentList.strings.length); 
-    
-    //Stores randomly generated affirmation in a variable
-	let currentAff = currentList.strings[index];
-	
-    //Finds HTML div with an ID of aff-text and sets its content to the affirmation variable
-    let affText = document.querySelector('#affText');
-	affText.innerHTML = currentAff;
-}
-
+//Creates a global variable currentCat for "state", i.e. which category has been selected
+var currentCat = "";
+var currentList = "";
 
 // * NAVIGATION FUNCTIONS //
 
@@ -149,7 +92,6 @@ function goToAff() {
 		body.classList.remove("howto-open");
 	} 
 	body.classList.add("aff-open");
-	alert(currentCat);
 }
 
 //A button that calls this function opens the how-to screen.
@@ -157,3 +99,57 @@ function goToAff() {
 function goToHowTo() {
 	body.classList.add("howto-open");
 }
+
+function generateNew() {
+    //Generates a random index to retrieve a random affirmation from the list
+    let index = Math.floor(Math.random()*currentList.strings.length); 
+    
+    //Stores randomly generated affirmation in a variable
+	let currentAff = currentList.strings[index];
+	
+    //Finds HTML div with an ID of aff-text and sets its content to the affirmation variable
+    let affText = document.querySelector('#affText');
+	affText.innerHTML = currentAff;
+}
+
+//Updates global variable, list of affirmations, and affirmation page heading based on button click
+// TODO: make updateCurrentCat() its own function and place it inside big-picture function!
+function updateCurrentCat(data) {
+	currentCat = data;
+	currentList = affirmations.filter(item => item.category == currentCat)[0];
+	var affCat = document.querySelector('#affCat');
+	var affHeading = `Positive affirmations for ${currentCat}`;
+	affCat.innerHTML = affHeading;
+	generateNew();
+	goToAff();
+}
+
+// * CATEGORIES (MAIN) SCREEN //
+
+//Creates a button element for each item in the affirmations object where "strings" is not empty
+var catButtons = `
+	${affirmations
+		.filter(item => item.strings.length > 0)
+		.map(item => 
+			`<button type="button" id="${item.category}" class="cat-button" style="background-color: ${item.color}" onclick="updateCurrentCat('${item.category}')">
+				<h3 class="cat-text">${item.category}</h3>
+			</button>
+			`)
+		.join('')}
+`;
+
+//Displays the button elements
+var catBtnContainer = document.querySelector('#catButtons');
+catBtnContainer.innerHTML = catButtons;
+
+
+// * AFFIRMATION SCREEN //
+
+// ? filter for current category and assign to new var
+// ? map over obj and format to just be array of strings?
+// ? then can use that index thingy
+
+
+
+
+
